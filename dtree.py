625b20e23f534426744bebd4c3ceeb873e747e6a
Paris <parismollochristondis@gmail.com>
2020-01-14 20:04:27 +0100

    - add find best split function

--- dtree.py
+++ dtree.py
@@ -83,3 +83,24 @@ print(gini(no_mixing))
 def info_gain(left, right, current_uncertainty):
     p = float(len(left)) / (len(left) + len(right))
     return current_uncertainty - p * gini(left) - (1 - p) * gini(right)
+
+def find_best_split(rows):
+    best_gain = 0
+    best_question = None
+    current_uncertainty = gini(rows)
+    n_features = len(rows[0]) - 1
+
+    for col in range(n_features):
+        values = set([row[col] for row in rows])
+        for val in values:
+            question = Question(col, val)
+            true_rows, false_rows = partition(rows, question)
+            if len(true_rows) == 0 or len(false_rows) == 0:
+                continue
+            gain = info_gain(true_rows, false_rows, current_uncertainty)
+            if gain>= best_gain:
+                best_gain, best_question = gain, question
+    return best_gain, best_question
+
+best_gain, best_question = find_best_split(training_data)
+print(f'Here is the best gain {best_gain} with this question {best_question}')
